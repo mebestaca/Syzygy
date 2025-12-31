edd6901d235b18400bd7eb65baf9d79bb576075b
Marc Edison Estaca <mebestaca@gmail.com>
2025-12-30 21:36:20 -0700

    added CustomFrame

--- syzygy/src/syzygy/lib/FrameCustom.java
+++ syzygy/src/syzygy/lib/FrameCustom.java
@@ -8,15 +8,14 @@ public class FrameCustom {
 	private String horizontal_connector;
 	private String vertical_connector;
 	
-	private  FrameCustom(String upper_left_corner, String upper_right_corner, String lower_left_corner,
-			String lower_right_corner, String horizontal_connector, String vertical_connector) {
+	private FrameCustom(Builder builder) {
 		super();
-		this.upper_left_corner = upper_left_corner;
-		this.upper_right_corner = upper_right_corner;
-		this.lower_left_corner = lower_left_corner;
-		this.lower_right_corner = lower_right_corner;
-		this.horizontal_connector = horizontal_connector;
-		this.vertical_connector = vertical_connector;
+		this.upper_left_corner = builder.upper_left_corner;
+		this.upper_right_corner = builder.upper_right_corner;
+		this.lower_left_corner = builder.lower_left_corner;
+		this.lower_right_corner = builder.lower_right_corner;
+		this.horizontal_connector = builder.horizontal_connector;
+		this.vertical_connector = builder.vertical_connector;
 	}
 	
 	public String getUpper_left_corner() {
@@ -36,5 +35,52 @@ public class FrameCustom {
 	}
 	public String getVertical_connector() {
 		return vertical_connector;
+	}
+	
+	public static class Builder {
+		private String upper_left_corner;
+		private String upper_right_corner;
+		private String lower_left_corner;
+		private String lower_right_corner;
+		private String horizontal_connector;
+		private String vertical_connector;
+		
+		public Builder() {
+			
+		}
+		
+		public Builder setUpperLeftCorner(String glyph) {
+			this.upper_left_corner = glyph;
+			return this;
+		}
+		
+		public Builder setUpperRightCorner(String glyph) {
+			this.upper_right_corner = glyph;
+			return this;
+		}
+		
+		public Builder setLowerLeftCorner(String glyph) {
+			this.lower_left_corner = glyph;
+			return this;
+		}
+		
+		public Builder setLowerRightCorner(String glyph) {
+			this.lower_right_corner = glyph;
+			return this;
+		}
+		
+		public Builder setHorizontalConnector(String glyph) {
+			this.horizontal_connector = glyph;
+			return this;
+		}
+		
+		public Builder setVerticalConnector(String glyph) {
+			this.vertical_connector = glyph;
+			return this;
+		}
+	
+		public FrameCustom build() {
+			return new FrameCustom(this);
+		}
 	}
 }
