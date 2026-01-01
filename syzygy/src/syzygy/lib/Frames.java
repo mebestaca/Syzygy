5b16137b0cdc46bae691eba0f1427418f117a554
Marc Edison Estaca <mebestaca@gmail.com>
2025-12-31 17:06:13 -0700

    double frame configuration

--- syzygy/src/syzygy/lib/Frames.java
+++ syzygy/src/syzygy/lib/Frames.java
@@ -1,15 +1,27 @@
 package syzygy.lib;
 
-public class Frames {
+public class Frames implements IFrames {
 	private static int left_pad = 0;
 	private static int right_pad = 0;
 	
-	private static String upper_left_corner = "╔";
-	private static String upper_right_corner = "╗";
-	private static String lower_left_corner = "╚";
-	private static String lower_right_corner = "╝";
-	private static String horizontal_connector = "═";
-	private static String vertical_connector = "║";
+	private String upper_left_corner;
+	private String upper_right_corner;
+	private String lower_left_corner;
+	private String lower_right_corner;
+	private String horizontal_connector;
+	private String vertical_connector;
+	
+	@Override
+	public void configureStyle(FrameTypes type, FrameCustom custom) {
+		if (FrameTypes.DOUBLE_FRAME == type) {
+			this.upper_left_corner = "╔";
+			this.upper_right_corner = "╗";
+			this.lower_left_corner = "╚";
+			this.lower_right_corner = "╝";
+			this.horizontal_connector = "═";
+			this.vertical_connector = "║";
+		}
+	}
 	
 	
 }
