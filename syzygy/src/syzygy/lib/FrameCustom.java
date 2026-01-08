7584f556701becf22a829a008a20e3c3e44b5a2a
Marc Edison Estaca <mebestaca@gmail.com>
2026-01-07 19:15:40 -0700

    cross conenctors

--- syzygy/src/syzygy/lib/FrameCustom.java
+++ syzygy/src/syzygy/lib/FrameCustom.java
@@ -7,6 +7,11 @@ public class FrameCustom {
 	private String lower_right_corner;
 	private String horizontal_connector;
 	private String vertical_connector;
+	private String crossConnector;
+	private String leftVerticalConnector;
+	private String rightVerticalConnector;
+	private String topHorizontalConnector;
+	private String bottomhorizontalConnector;
 	
 	private FrameCustom(Builder builder) {
 		super();
@@ -16,6 +21,11 @@ public class FrameCustom {
 		this.lower_right_corner = builder.lower_right_corner;
 		this.horizontal_connector = builder.horizontal_connector;
 		this.vertical_connector = builder.vertical_connector;
+		this.crossConnector = builder.crossConnector;
+		this.leftVerticalConnector = builder.leftVerticalConnector;
+		this.rightVerticalConnector = builder.rightVerticalConnector;
+		this.topHorizontalConnector = builder.topHorizontalConnector;
+		this.bottomhorizontalConnector = builder.bottomhorizontalConnector;
 	}
 	
 	public String getUpperLeftCorner() {
@@ -37,6 +47,26 @@ public class FrameCustom {
 		return vertical_connector;
 	}
 	
+	public String getCrossConnector() {
+		return crossConnector;
+	}
+
+	public String getLeftVerticalConnector() {
+		return leftVerticalConnector;
+	}
+
+	public String getRightVerticalConnector() {
+		return rightVerticalConnector;
+	}
+
+	public String getTopHorizontalConnector() {
+		return topHorizontalConnector;
+	}
+
+	public String getBottomhorizontalConnector() {
+		return bottomhorizontalConnector;
+	}
+
 	public static class Builder {
 		private String upper_left_corner;
 		private String upper_right_corner;
@@ -44,6 +74,11 @@ public class FrameCustom {
 		private String lower_right_corner;
 		private String horizontal_connector;
 		private String vertical_connector;
+		private String crossConnector;
+		private String leftVerticalConnector;
+		private String rightVerticalConnector;
+		private String topHorizontalConnector;
+		private String bottomhorizontalConnector;
 		
 		public Builder() {
 			
@@ -76,6 +111,31 @@ public class FrameCustom {
 		
 		public Builder setVerticalConnector(String glyph) {
 			this.vertical_connector = glyph;
+			return this;
+		}
+		
+		public Builder setCrossConnector(String glyph) {
+			this.crossConnector = glyph;
+			return this;
+		}
+		
+		public Builder setLeftVerticalConnector(String glyph) {
+			this.leftVerticalConnector = glyph;
+			return this;
+		}
+		
+		public Builder setRightVerticalConnector(String glyph) {
+			this.rightVerticalConnector = glyph;
+			return this;
+		}
+		
+		public Builder setTopHorizontalConnector(String glyph) {
+			this.topHorizontalConnector = glyph;
+			return this;
+		}
+		
+		public Builder setBottomhorizontalConnector(String glyph) {
+			this.bottomhorizontalConnector = glyph;
 			return this;
 		}
 	
